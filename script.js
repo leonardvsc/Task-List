e16524b3949e5e6ce000410cc15bd9a64a201e31
Leonardo Campos <leonardoworld@outlook.com>
2024-04-10 20:47:52 -0300

    parentElementRemove

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ function limpaInput() {
 
 function criaBotaoApagar(li) {
   const botaoApagar = document.createElement("i");
-  botaoApagar.classList.add("bi", "bi-trash3");
+  //botaoApagar.classList.add("bi", "bi-trash3");
+  //outra forma de adicionar classes a um elemento e qualquer propriedade do elemento.
+  botaoApagar.setAttribute("class", "bi bi-trash3 btn-apagar");
+  botaoApagar.setAttribute("title", "Delete task");
   li.appendChild(botaoApagar);
 }
+
 function criaTarefa(textoInput) {
   const li = criaLi();
   li.innerText = textoInput;
@@ -39,5 +43,13 @@ inputTarefa.addEventListener("keypress", function (e) {
   if (e.keyCode === 13) {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
+  }
+});
+
+document.addEventListener("click", function (e) {
+  const element = e.target; // Seleciona o elemento que disparou o evento click
+  // Se o elemento tiver a classe btn-apagar
+  if (element.classList.contains("btn-apagar")) {
+    element.parentElement.remove(); // Remove o elemento pai do elemento que o disparou
   }
 });
